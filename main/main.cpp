25384d945bac6e5470a4a98d0896fb1fca6c347e
winiarcc <winiarcc@lafayette.edu>
2018-04-17 00:02:45 -0400

    initial commit

---
+++ main/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <httplib.h>
+
+int main() {
+
+	using namespace httplib;
+
+	Server app;
+
+	app.get("/test", [](const Request& req, Response& res){
+		res.set_content("HelloWorld!", "text/plain");
+	});
+
+	app.listen("localhost", 8000);
+
+	return 0;
+}
