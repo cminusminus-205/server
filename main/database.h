79062de1f22f063cb5557bb57e238d96ead976b5
winiarcc <winiarcc@lafayette.edu>
2018-04-18 22:53:31 -0400

    Got Database Working

---
+++ main/database.h
@@ -0,0 +1,100 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <sqlite3.h>
+#include <list>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <map>
+
+enum {BUSY, DONE} status;
+
+class Query;
+
+int sqlite_callback(void *NotUsed, int argc, char** argv, char** azColName);
+std::list<Query*> operation_queue;
+
+class Query {
+public:
+	Query(std::string _SQL) {
+		SQL = _SQL;
+		finished = false;
+	}
+	~Query() {}
+
+	void write_back(std::string row, bool done) {
+		if(done) {
+			finished = true;
+		} else {
+			// result.push_back(row);
+		}
+	}
+
+	std::string result;
+
+	std::string SQL;
+	bool finished;
+};
+
+class Database {
+public:
+	Database(std::string path){
+		int status = sqlite3_open(path.c_str(), &db);
+	}
+	~Database(){}
+
+	void execute_next(){
+		std::stringstream output;
+
+		sqlite3_stmt* stmt;
+		int rc;
+
+		Query* operation = operation_queue.front();
+		sqlite3_prepare_v2(db, operation -> SQL.c_str(), -1, &stmt, NULL);
+
+		for(int i=0; i<sqlite3_column_count(stmt); i++) {
+			output << sqlite3_column_name(stmt, i) << ",";
+		}
+
+		output << std::endl;
+
+		while ( (rc = sqlite3_step(stmt)) == SQLITE_ROW) {  
+			for(int i=0; i<sqlite3_column_count(stmt); i++) {
+				const char* col = reinterpret_cast<const char*>(sqlite3_column_text(stmt, i));
+				output << (col ? std::string(col) : "NULL") << ", ";
+				// output << (sqlite3_column_text(stmt, i) ? (std::string s(sqlite3_column_text(stmt, i))) : "NULL") << ",";
+			}
+
+			output << std::endl;
+		}
+
+		operation_queue.front() -> result = output.str();
+		if(!operation_queue.empty()) operation_queue.pop_front();	
+	}
+
+	sqlite3* db;
+	char* error_message;
+
+	Database* operator<<(Query* operation) {
+		operation_queue.push_back(operation);
+		this -> execute_next();
+		return this;
+	}
+};
+
+int sqlite_callback(void *NotUsed, int argc, char** argv, char** azColName){
+	std::cout << "called" << std::endl;
+	std::stringstream output;
+	for(int i = 0; i<argc; i++) {
+		output << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << "\n";
+	}
+
+	operation_queue.front() -> write_back(output.str(), false);
+	if(!operation_queue.empty()) operation_queue.pop_front();
+
+	return 0;
+}
+
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "httplib.h"
+#include <sqlite3.h>
+
+#include "database.h"
 
 int main() {
 
@@ -7,11 +10,28 @@ int main() {
 
 	Server app;
 
+	//open a connection to our database
+	Database* db = new Database("/Users/connorwiniarczyk/Desktop/data.db");
+
+	app.post("/data/execute", [&](const auto& req, auto& res){
+		Query* query = new Query(req.body);
+		*db << query;
+
+		res.set_content(query -> result, "text/plain");
+	});
+
+	app.post("/data/add_user", [&](const auto& req, auto& res){
+		res.set_content("Post!", "text/plain");
+	});
+
 	app.get("/test", [](const Request& req, Response& res){
 		res.set_content("HelloWorld!", "text/plain");
 	});
 
-	app.listen("localhost", 8000);
+	app.listen("0.0.0.0", 8000);
+	
+	
+	
 
 	return 0;
 }
