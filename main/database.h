3615421b017bb3b80efca2bb76df9c61cb51fdd9
angelasy <38188310+angelasy@users.noreply.github.com>
2018-04-26 17:37:56 -0400

    Added column to tables

--- main/database.h
+++ main/database.h
@@ -19,34 +19,35 @@ class Query {
 public:
 	Query(std::string _SQL) {
 		SQL = _SQL;
+		finished = false;
 	}
 	~Query() {}
 
 	std::list<json> result;
 
 	std::string SQL;
+	bool finished;
 };
 
 class Database {
 public:
 	Database(std::string path){
 		int status = sqlite3_open(path.c_str(), &db);
-		std::cout << status;
 	}
 	~Database(){}
 
-	void execute(Query* operation){
+	void execute_next(){
 		//store our output as a list of rows, with each row being a json object
 		std::list<json> output;
 
 		//create the sql stmt we want to execute
 		sqlite3_stmt* stmt;
 
-		// Query* operation = operation_queue.front();
+		Query* operation = operation_queue.front();
 		sqlite3_prepare_v2(db, operation -> SQL.c_str(), -1, &stmt, NULL);
 
 		while ( sqlite3_step(stmt) == SQLITE_ROW) {
-			// add a new row ot the list
+			//add a new row ot the list
 			
 			json row;
 
@@ -68,16 +69,19 @@ public:
 			output.push_back(row);
 		}
 
-		// store the result
-		operation -> result = output;
+		//store the result
+		operation_queue.front() -> result = output;
+
+		//remove this query from the queue
+		if(!operation_queue.empty()) operation_queue.pop_front();	
 	}
 
 	// wipes the database
 	void wipe(){
 		*this << new Query("DROP TABLE IF EXISTS users;");
 		*this << new Query("DROP TABLE IF EXISTS emergencies;");
-		*this << new Query("CREATE TABLE users (id INTEGER UNIQUE PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL, privileges TEXT NOT NULL);");
-		*this << new Query("CREATE TABLE emergencies (id INTEGER UNIQUE PRIMARY KEY, type TEXT NOT NULL, latitude DOUBLE, longitude DOUBLE);");
+		*this << new Query("CREATE TABLE users (id INTEGER UNIQUE PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL, privileges TEXT NOT NULL, phone TEXT, workAd TEXT, homeAd TEXT, workZip INTEGER, homeZip INTEGER, workType TEXT, workStation TEXT, stationZip INTEGER, verify INTEGER, identity INTEGER);");
+		*this << new Query("CREATE TABLE emergencies (id INTEGER UNIQUE PRIMARY KEY, type TEXT NOT NULL, latitude DOUBLE, longitude DOUBLE, time TEXT, status INTEGER, location TEXT);");
 
 		// the database should always have a god user
 		*this << new Query("INSERT INTO users (email, first_name, password_hash, privileges) VALUES ('god@user.com', 'god', 'password', 'GOD');");
@@ -87,8 +91,8 @@ public:
 	char* error_message;
 
 	Database* operator<<(Query* operation) {
-		// operation_queue.push_back(operation);
-		this -> execute(operation);
+		operation_queue.push_back(operation);
+		this -> execute_next();
 		return this;
 	}
 };
